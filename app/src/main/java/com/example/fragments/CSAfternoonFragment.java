72d0e447fd3f1c57441e846d4cd5f101660d8322
Aliza Tariq <alizatariq1122@gmail.com>
2023-02-12 12:22:20 +0500

    Fragments

--- app/src/main/java/com/example/fragments/CSAfternoonFragment.java
+++ app/src/main/java/com/example/fragments/CSAfternoonFragment.java
@@ -1,4 +1,32 @@
 package com.example.fragments;
+import android.os.Bundle;
 
-public class CSAfternoonFragment {
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class CSAfternoonFragment extends Fragment {
+
+
+
+    public CSAfternoonFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_c_s_afternoon, container, false);
+        TextView t1=view.findViewById(R.id.textInput);
+        t1.setText("Hi");
+
+
+        return view;
+    }
 }
