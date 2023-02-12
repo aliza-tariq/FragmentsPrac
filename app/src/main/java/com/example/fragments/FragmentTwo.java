45a409390a9df100164605266ab118224fcbb443
Aliza Tariq <alizatariq1122@gmail.com>
2023-02-12 12:30:51 +0500

    Fragments

--- app/src/main/java/com/example/fragments/FragmentTwo.java
+++ app/src/main/java/com/example/fragments/FragmentTwo.java
@@ -1,4 +1,30 @@
 package com.example.fragments;
+import android.os.Bundle;
 
-public class FragmentTwo {
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FragmentTwo#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FragmentTwo extends Fragment {
+
+    public FragmentTwo() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_two, container, false);
+
+        return view;
+
+    }
 }
