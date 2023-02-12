61cc1839e485c1f66733ffa8dea9c8a2b758c9b7
Aliza Tariq <alizatariq1122@gmail.com>
2023-02-12 12:23:50 +0500

    Fragments

--- app/src/main/java/com/example/fragments/FragmentOne.java
+++ app/src/main/java/com/example/fragments/FragmentOne.java
@@ -1,14 +1,35 @@
 package com.example.fragments;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class FragmentOne extends AppCompatActivity {
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class FragmentOne extends Fragment {
+
+    Button buttonFinish;
+
+    public FragmentOne() {
+        // Required empty public constructor
+    }
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment_one);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_one, container, false);
+
+        buttonFinish = view.findViewById(R.id.buttonFinish);
+        buttonFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().finish();
+            }
+        });
+        return view;
     }
 }
