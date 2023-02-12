be4f6ace7b2ab15566f79a83e6e45ede708390d6
Aliza Tariq <alizatariq1122@gmail.com>
2023-02-12 12:20:53 +0500

    Fragments

--- app/src/main/java/com/example/fragments/CSMFragment.java
+++ app/src/main/java/com/example/fragments/CSMFragment.java
@@ -1,30 +1,4 @@
 package com.example.fragments;
-import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-
-public class CSMFragment extends Fragment {
-
-
-
-    public CSMFragment() {
-        // Required empty public constructor
-    }
-
-
-
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_c_s_m, container, false);
-
-        return view;
-    }
+public class CSMFragment {
 }
